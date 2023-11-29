d07dfad51ecde63c72ddc6f61f9e8355d2f026b5
chufuyun <88359419+chufuyun@users.noreply.github.com>
2023-11-30 06:07:02 +0800

    Update A3A_pileup.py

--- A3A_pileup.py
+++ A3A_pileup.py
@@ -3,15 +3,13 @@ import pysam
 from collections import defaultdict
 
 def main(args):
-    # 打开输入的BAM文件
+   
     input_bam = pysam.AlignmentFile(args.input, "rb")
 
-    # 创建输出的BAM文件
     output_bam = pysam.AlignmentFile(args.output, "wb", template=input_bam)
 
     coverage_dict = defaultdict(lambda: defaultdict(int))
 
-    # 遍历BAM文件的每个read
     for read in input_bam:
         tags = dict(read.tags)
         if "XM" in tags:
@@ -26,7 +24,7 @@ def main(args):
                     else:
                         coverage_dict[site_position_strand]['upper'] += 1
 
-    # 输出位点的统计信息到文本文件
+  
     with open(args.summary, "w") as f:
         f.write("chr\tpos\tstrand\tuppercase\tlowercase\tconversion rate\n")
         for site_position_strand, counts in coverage_dict.items():
@@ -37,24 +35,22 @@ def main(args):
             conversion_rate = lowercase / total if total > 0 else 0
             f.write(f"{chr}\t{pos}\t{strand}\t{uppercase}\t{lowercase}\t{conversion_rate:.2f}\n")
 
-    # 根据覆盖率和转换率阈值筛选reads
-    # input_bam.reset()  # 重置BAM文件的读取指针
-
+    # input_bam.reset() 
     # for read in input_bam:
     #     tags = dict(read.tags)
     #     if "XM" in tags:
     #         xm = tags["XM"]
     #         position = read.reference_start
-    #         strand = '+' if read.flag in {99, 83} else '-'  # 确定链方向
+    #         strand = '+' if read.flag in {99, 83} else '-'  
     #         for i, char in enumerate(xm):
-    #             site_position_strand = f"{read.reference_name}:{position + i}:{strand}"  # 创建包含位点和链方向的键
+    #             site_position_strand = f"{read.reference_name}:{position + i}:{strand}" 
     #             if char.islower():
     #                 coverage = coverage_dict[site_position_strand]['lower'] + coverage_dict[site_position_strand]['upper']
     #                 total_conversions = coverage_dict[site_position_strand]['lower']
     #                 conversion_rate = total_conversions / coverage if coverage > 0 else 0
     #                 if coverage > 1 and conversion_rate > 0.01:
     #                     output_bam.write(read)
-    #                     break  # 如果read满足条件，写入到输出BAM文件并跳出循环
+    #                     break  
 
     # input_bam.close()
     # output_bam.close()
